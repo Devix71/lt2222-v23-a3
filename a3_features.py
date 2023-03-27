41ec805740f2aa24248ba2b291773bfd6b1ca0bb
Devix71 <nitronitro60@yahoo.com>
2023-03-27 19:18:40 +0200

    Part 2 and 3 done (to the best of my ability)

--- a3_features.py
+++ a3_features.py
@@ -1,51 +1,63 @@
 import os
-import sys
-import glob
 import re
+import string
 import argparse
-import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
+# hot encoding the author names based on the selected author function
+def encode_author(df, author_name):
+    df['authors'] = df['authors'].apply(lambda x: 1 if x == author_name else 2)
+    return df
+
+
+# text preprocessing function
+def text_process(tex):
+
+    # removing punctuation marks
+    nopunct=[char for char in tex if char not in string.punctuation]
+    nopunct=''.join(nopunct)
+    return nopunct
+
+# author and email content extractor function
 def extract_author_and_content(email_string):
     list_s = []
     list_a = []
 
+    # regex that extracts email author based on anchor words
     author_content = re.findall(r'(?<=From:\s)(?:(?!\.com)[^<\n])+ ', email_string)
+
+    # regex that extracts email content based on anchor words
     email_content = re.findall(r'Subject:\t.*\n*.*\n?.*|Subject:\s.*\n.*\n.*\n?.*|pst\n*.*\n*.*', email_string)
     
 
     if email_content:
         
-        email_content = [s.strip().replace("\n", "").replace("pst", "").replace("\t", "").replace("-----Original Message-----","") for s in email_content]
+        # eliminating redundant and boilerplate characters and substrings
+        email_content = [s.strip().replace("\n", "").replace("pst", "").replace("\t", "").replace("-----Original Message-----","").replace("RE:","").replace("Subject:","") for s in email_content]
         
-        
+        # removing redundant words from email content
         substring = "charset=us-ascii"
         substring2 = "Mime-Version"
 
-        
-
         email_content = [elem for elem in email_content if substring not in elem]
         email_content = [elem for elem in email_content if substring2 not in elem]
-        print(email_content)
+
         list_s.extend(email_content)
-        #print(list_s)
+
+        # eliminating redundant and boilerplate characters
         author_content = [s.strip().replace("\t", "") for s in author_content]
-        #print(author_content)
+
 
         list_a.extend(author_content)
-
-
-        
-    #print(len(list_s))
-    #print(len(author_email))
-    #email_content = email_string[content_start:] 
 
     return author_content, email_content
 
 
 if __name__ == "__main__":
+
+    # defining the script arguments
     parser = argparse.ArgumentParser(description="Convert directories into table.")
     parser.add_argument("inputdir", type=str, help="The root of the author directories.")
     parser.add_argument("author", type=str, help="The name of the author to be classified.")
@@ -55,15 +67,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("Reading {}...".format(args.inputdir))
-    if args.author == "all":
-        path = args.inputdir
-    else:
-        path =  args.inputdir + args.author +"/"       #'/home/devix/Documents/School/LT2222/Assignment3/lt2222-v23/enron_sample/panus-s/'
-    #print(glob.glob(f'{path}*'))
-    # Read the documents
+
+    path = args.inputdir 
+
+
     authors = []
     texts = []
 
+    # File parser for reading the data in the subfolder's files
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.'):
@@ -75,15 +86,11 @@ if __name__ == "__main__":
   
     
     print("Constructing table with {} feature dimensions and {}% test instances...".format(args.dims, args.testsize))
-    
-
-    print([len(s) for s in authors])
-
-    print([len(x) for x in texts])
 
     columns = ['authors', 'text']
     df = pd.DataFrame(columns=columns)
     
+    # creating the initial dataframe with the email's content and author
     for i in range(len(authors)):
         if len(texts[i]) > len(authors[i]):
             for y in range(len(authors[i])):
@@ -93,38 +100,45 @@ if __name__ == "__main__":
             for y in range(len(texts[i])):
                 new_row = {'authors': f'{authors[i][y]}', 'text': f'{texts[i][y]}'}
                 df = df.append(new_row, ignore_index=True)
-
-
- 
-    #print(df)
-
-    # Vectorize the documents
     
+    # preprocessing the authors' names
+    df["authors"] = df.apply(lambda row: text_process(row.authors), axis = 1)
+    
+    # encoding the authors' names
+    df = encode_author(df, args.author)
+    df.dropna(subset=['text', 'authors'], inplace=True)
+
+    # initialising vectoriser for inputed dimensions
     vectorizer_x = TfidfVectorizer(max_features=args.dims)
-    vectorizer_y = TfidfVectorizer(max_features=1)
 
     X = vectorizer_x.fit_transform(df.text)
-    Y = vectorizer_y.fit_transform(df.authors)
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=args.testsize/100, random_state=42)
-    #print(y_test.reset_index(drop=True))
-    # Convert to pandas dataframe and write to file
-    df_final_X = pd.DataFrame(X_train.toarray(), columns=vectorizer_x.get_feature_names())
-    df_final_Y = pd.DataFrame(y_train.toarray(), columns=vectorizer_y.get_feature_names())
-    df_final = pd.concat([df_final_X, df_final_Y], axis=1)
-    df_final.insert(0, "type", "Train")
-    #df_final.to_csv(args.author + "_table_train.csv", index=False)
 
+    Y = df.authors
+
+    # splitting the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=args.testsize/100, random_state=42)
+
+    # convert the training data to pandas dataframe and write to file
+    df_final_X = pd.DataFrame(X_train.toarray(), columns=vectorizer_x.get_feature_names())
+    df_final_Y = pd.DataFrame(y_train, columns=["authors"])
+    df_final = pd.concat([df_final_X, df_final_Y], axis=1)
+
+    # marking the rows as training data
+    df_final.insert(0, "type", "Train")
+
+    # convert the testing data to pandas dataframe and write to file
     df_final_test_X = pd.DataFrame(X_test.toarray(), columns=vectorizer_x.get_feature_names())
-    df_final_test_Y = pd.DataFrame(y_test.toarray(), columns=vectorizer_y.get_feature_names())
+    df_final_test_Y = pd.DataFrame(y_test, columns=["authors"])
     df_final_test = pd.concat([df_final_test_X, df_final_test_Y], axis=1)
+
+    # marking the rows as training data
     df_final_test.insert(0, "type", "Test")
-    #df_final_test.to_csv(args.author + "_table_test.csv", index=False)
+
+    # cleaning the dataframe and outputting it to .csv
     df_concat = pd.concat([df_final, df_final_test])
+    df_concat = df_concat.fillna(0.0)
+    df_concat = df_concat.drop(df_concat[df_concat['authors'] == 0.0].index)
+    print(df_concat[df_concat['authors'] == 1])
     df_concat.to_csv(args.author + "_table_vect.csv", index=False)
 
     print("Done!")
-
-
-
-    #python a3_features.py /home/devix/Documents/School/LT2222/Assignment3/lt2222-v23/enron_sample/panus-s/ tester 3
--- a3_model_3.py
+++ a3_model_3.py
@@ -1,15 +1,15 @@
-
 import torch
 import torch.optim as optim
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
-import torch.nn.functional as F
 import argparse
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
+
+# defining a dataset class for storing the data from the file
 class MyDataset(Dataset):
     def __init__(self, samples) -> None:
         """ is called, when creating the object: e.g. dataset = MyDataset(samples)
@@ -28,9 +28,10 @@ class MyDataset(Dataset):
         """
         return len(self.samples)
 
-
+# defining the model class for the neural network model
 class Model(nn.Module):
-    def __init__(self, num_hidden_layers=1, hidden_size=5, nonlinearity_func=nn.ReLU) -> None:
+    # adding options to the model for hidden layer, choice of two non-linearities and size of the hidden layer  
+    def __init__(self, dimensions, num_hidden_layers=1, hidden_size=5, nonlinearity_func=nn.ReLU) -> None:
         """ is called when model is created: e.g model = Model()
             - definition of layers
         """
@@ -39,7 +40,7 @@ class Model(nn.Module):
         self.hidden_size = hidden_size
         self.nonlinearity_func = nonlinearity_func
 
-        self.input_layer = nn.Linear(3, hidden_size)
+        self.input_layer = nn.Linear(dimensions, hidden_size)
         self.hidden_layers = nn.ModuleList([
             nn.Sequential(
                 nn.Linear(hidden_size, hidden_size),
@@ -47,6 +48,8 @@ class Model(nn.Module):
             ) for _ in range(num_hidden_layers)
         ])
         self.output = nn.Linear(hidden_size, 3)
+
+        # implementing the LogSoftMax for outputting the distribution of probabilities
         self.LogSoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, data):
@@ -59,7 +62,7 @@ class Model(nn.Module):
             after_input_layer = after_hidden_layer
         output = self.output(after_hidden_layer)
 
-        return self.LogSoftmax(output), output
+        return self.LogSoftmax(output)
 
 if __name__ == '__main__':
 
@@ -70,7 +73,6 @@ if __name__ == '__main__':
     parser.add_argument("hidden_size", type=int, help="Number of neurons in each hidden layer of the neural network.")
     parser.add_argument("nonlinearity_func", type=str, help="Non-linearity function to be applied in each hidden layer. Choose between 'relu' and 'sigmoid'.")
 
-
     # Add options here for part 3 -- hidden layer and nonlinearity,
     # and any other options you may think you want/need.  Document
     # everything.
@@ -79,129 +81,116 @@ if __name__ == '__main__':
 
     df_csv =  pd.read_csv(args.featurefile)
 
-
-    # Select rows with type "Train" and create a new dataframe "df_train"
+    # selecting rows with type "Train" and creating the training dataframe "df_train"
     df_train = df_csv.loc[df_csv['type'] == 'Train']
     df_train = df_train.drop(['type'], axis=1)
-    column_names_x = df_train.columns[:3]
+    column_names_x = df_train.columns[:(len(df_train.columns)-1)]
 
-    column_names_y = df_train.columns[-3:]
+    column_names_y = df_train.columns[-1:]
 
-    # Select rows with type "Test" and create a new dataframe "df_test"
+    # selecting rows with type "Test" and creating the testing dataframe "df_test"
     df_test = df_csv.loc[df_csv['type'] == 'Test']
     df_test = df_test.drop(['type'], axis=1)
 
-    y_train = df_train.iloc[:, -3:].values
-    X_train = df_train.iloc[:, :3].values
+    y_train = df_train.iloc[:, -1:].values.flatten()
 
+    X_train = df_train.iloc[:, :(len(df_train.columns)-1)].values
+
+    # formating the data into a list of elements of shape tuple(feature vector,label)
     sample_list = []
-
     for i in range(len(y_train)):
-        sample_list.append((list(X_train[i]),list(y_train[i]) ))
+        sample_list.append((list(X_train[i]), y_train[i]))
 
 
 
-    
+    # loading the data into the dataset class
     dataset = MyDataset(samples = sample_list)
+
     # creation of dataloader for batching and shuffling of samples
     dataloader = DataLoader(dataset,
                             batch_size=4,
                             shuffle=True,
                             collate_fn=lambda x: x)
-    
-
-    model = Model(num_hidden_layers=2, hidden_size=10, nonlinearity_func=nn.Tanh)
-
+    model = Model(dimensions=len(df_train.columns)-1,num_hidden_layers=2, hidden_size=10, nonlinearity_func=nn.Tanh)
 
     # optimizer defines the method how the model is trained
-    optimizer = optim.Adam(model.parameters(), lr=0.002)
+    optimizer = optim.Adam(model.parameters(), lr=0.003)
 
     # the loss function calculates the 'difference' between the models output and the ground truth
     loss_function = nn.CrossEntropyLoss()
 
     # number of epochs = how often the model sees the complete dataset
-    for epoch in range(4):
+    for epoch in range(5):
         total_loss = 0
+
         # loop through batches of the dataloader
         for i, batch in enumerate(dataloader):
-            # turn list of complete samples into list of inputs and list of ground_truths
-            # both lists are then converted into a tensor (matrix), which can be used by PyTorch
+
+            # turning the list of complete samples into a list of inputs and a list of ground_truths
+            # converting both lists into a tensor (matrix), to be used by PyTorch
             model_input = torch.Tensor([sample[0] for sample in batch])
-            ground_truth = torch.Tensor([sample[1] for sample in batch]).long()
+            ground_truth = torch.clamp(torch.Tensor([sample[1] for sample in batch]), min=1, max=2)  # subtract 1 from each label
 
-            # send your batch of sentences to the forward function of the model
-            log_probs, output = model(model_input)
+            # sending the batch of sentences to the forward function of the model
+            output = model(model_input)
 
-            # compare the output of the model to the ground truth to calculate the loss
+            # comparing the output of the model to the ground truth and calculating the loss
             # the lower the loss, the closer the model's output is to the ground truth
-            loss = loss_function(output, ground_truth.float())
+            loss = loss_function(output, ground_truth.long())
 
-            # print average loss for the epoch
+            # printing average loss for the epoch
             total_loss += loss.item()
             print(f'epoch {epoch},', f'batch {i}:', round(total_loss / (i + 1), 4), end='\r')
 
-            # train the model based on the loss:
-            # compute gradients
+            # training the model based on the loss:
+
+            # computing gradients
             loss.backward()
-            # update parameters
+            # updating parameters
             optimizer.step()
-            # reset gradients
+            # reseting gradients
             optimizer.zero_grad()
+        print()
     
-    # Create the training data
-
-    y_test = df_test.iloc[:, -3].values
-    X_test = df_test.iloc[:, :3].values
-
+    # Creating the testing data
+    y_test = df_test.iloc[:, -1].values.flatten()
+    X_test = df_test.iloc[:, :(len(df_train.columns)-1)].values
 
     sample_list_test = []
     for i in range(len(y_test)):
-        sample_list_test.append((list(X_train[i]),list(y_train[i]) ))
-
-    #print(sample_list_test)
+        sample_list_test.append((list(X_test[i]), y_test[i]))
 
     dataset_test = MyDataset(samples = sample_list_test)
 
-    # create a new DataLoader for the test dataset
+    # creating a new DataLoader for the test dataset
     test_dataloader = DataLoader(dataset_test,
-                                batch_size=1,
+                                batch_size=4,
                                 shuffle=False,
                                 collate_fn=lambda x: x)
+    
+    # predicting labels for the test set
     model.eval()
     with torch.no_grad():
         model_input = torch.Tensor(X_test)
         output = model(model_input)
-        
-        y_pred = torch.argmax(output[0], dim=1).numpy()
-        probabilities = F.softmax(output[0], dim=1)
-        class_probabilities = probabilities.numpy()
-        print(class_probabilities)
+        y_pred = torch.argmax(output, dim=1).numpy()
 
-    # Compute confusion matrix
+    # computing the confusion matrix
+    labels = [1,2]
+    cm = confusion_matrix(y_test, y_pred,labels = [1,2])
+    
 
-    # Get the predicted labels
-    y_pred = np.argmax(class_probabilities, axis=1)
-
-    # Compute confusion matrix
-    cm = confusion_matrix(y_test, y_pred, labels=[0, 1, 2])
-
-
-    print(cm)
-    # Plot confusion matrix
-    plt.imshow(cm, cmap=plt.cm.Blues)
-    plt.title('Confusion matrix')
+    # plotting the confusion matrix
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
     plt.colorbar()
-    tick_marks = np.arange(3)
-    plt.xticks(tick_marks, column_names_y, rotation=45)
-    plt.yticks(tick_marks, column_names_y)
-    plt.xlabel('Predicted label')
-    plt.ylabel('True label')
-
-    # Display counts in confusion matrix
-    width, height = cm.shape
-    for x in range(width):
-        for y in range(height):
-            plt.annotate(str(cm[x][y]), xy=(y, x), 
-                        horizontalalignment='center',
-                        verticalalignment='center')
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.xticks(np.arange(len(labels)), labels, rotation=45)
+    plt.yticks(np.arange(len(labels)), labels)
+    plt.tight_layout()
     plt.show()
+
+    # printing the confusion matrix
+    print(cm)
+
